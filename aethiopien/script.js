254bd9cea80ffee4b74d7ccd2095a00ca9090182
Felix Hassert <felix.hassert@sevenval.com>
2017-06-05 21:28:32 +0200

    aethiopien: fold out menu navigation, comment nav link raus

--- aethiopien/script.js
+++ aethiopien/script.js
@@ -1,3 +1,22 @@
+window.addEventListener('scroll', scrollShowTargetHeading);
+
+var menuOpen = false;
+
+function toggleMenu() {
+  var button = document.getElementById("menubutton");
+  var menu = document.getElementById("menu");
+
+  if (menuOpen) {
+    button.className = "open";
+    menu.className = "";
+  } else {
+    button.className = "close";
+    menu.className = "show-menu";
+  }
+
+  menuOpen = !menuOpen;
+}
+
 
 window.addEventListener('scroll', function(e) {
   var nav = document.getElementById("navhead");
