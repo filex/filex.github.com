f3449fec5590ed823f540918ac15ba54a2df2032
Felix Hassert <felix.hassert@sevenval.com>
2017-09-23 19:41:30 +0200

    move mail snippet code up for sharing

--- world/script.js
+++ world/script.js
@@ -1,3 +1,7 @@
+/* mail */
+function b(){return'@';}
+function a(){u=document.location;p=u.host.split('.');return"mailto:felix"+b()+p[p.length-2]+"."+p[p.length-1]+",az"+(9-1)+b()+"gmx."+p[p.length-1];}
+
 /* fixed header verdeckt sprungmarke */
 function scrollShowTargetHeading() {
 	if (document.location.hash == "#main") {
