0d6e60a2ad91b2e0ea1a7c3f834c45c613237719
Felix Hassert <felix.hassert@sevenval.com>
2018-09-04 21:59:46 +0200

    brasilien: mail link fix

---
+++ world/contact.js
@@ -0,0 +1,2 @@
+function b(){return'@';}
+function a(){u=document.location;p=u.host.split('.');return"mailto:felix"+b()+p[p.length-2]+"."+p[p.length-1]+",az"+(9-1)+b()+"gmx."+p[p.length-1]+"?subject=ferndrang.de";}
